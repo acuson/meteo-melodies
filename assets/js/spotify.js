9277570f44775cca9f0268ddba39f7b303177689
Christine Nguyen <cnhnguyen59@gmail.com>
2021-12-06 20:46:22 -0500

    created html for player page

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -2,7 +2,8 @@ var playlists = [1,2,3,4,5,6,7,8,9,10,11,12]
 var output =''
 
 playlists.forEach(playlist => {
-    output+= "<img src='./assets/images/100placeholder.png' class='m-1' style='height: 100px; width: 100px'/>";
+    output+= `<img src='./assets/images/100placeholder.png' class='m-1' style='height: 100px; width: 100px' onclick='window.location.href='./player.html''/>`;
 })
 
 $('.playlists').html(output)
+
