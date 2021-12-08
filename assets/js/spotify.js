11c7b22401de600f7c1a643e657d4c0b6668f103
Kevin Lacson <lacsonky@gmail.com>
2021-12-07 22:06:15 -0500

    added some comments to spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,13 +1,14 @@
 const clientId = "7a0f37913ee7411a91763141d13810b5";
-const redirectUri = "http://127.0.0.1:5500/recsPage.html";
+const redirectUri = "http://127.0.0.1:5500/recsPage.html"; //Will need to be Github page
 let authorize = () => {
     location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`;
 };
 
 function getCode() {
+    // Help from this YouTube video: https://www.youtube.com/watch?v=1vR3m0HupGI&t=636s
     let code = null;
     const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+    const urlParams = new URLSearchParams(queryString); // Constructor
     code = urlParams.get("code");
     getToken(code);
 }
@@ -17,12 +18,12 @@ let getToken = code => {
         method: "POST",
         headers: {
             Authorization:
-                "Basic N2EwZjM3OTEzZWU3NDExYTkxNzYzMTQxZDEzODEwYjU6NmVlOTc4MjJjMTg2NDZkMjg5MGYyOGVjOGMwNzFiNDA=",
+                "Basic N2EwZjM3OTEzZWU3NDExYTkxNzYzMTQxZDEzODEwYjU6NmVlOTc4MjJjMTg2NDZkMjg5MGYyOGVjOGMwNzFiNDA=", // base64 encoded from Postman
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         },
         redirect_uri: redirectUri,
         code: code,
-        body: "grant_type=client_credentials",
+        body: "grant_type=client_credentials", // oauth with PKCE preferred
     })
         .then(response => response.json())
         .then(data => {
@@ -52,11 +53,13 @@ function queryPlaylists() {
         .then(response => {
             if (response.status === 401) {
                 getToken();
+                // Help from: https://www.delftstack.com/howto/javascript/javascript-wait-for-function-to-finish/
                 setTimeout(() => {
-                    queryPlaylists()
+                    queryPlaylists(); // Waits for new token before next fetch attempt
                 }, 1000);
             }
-            return response.json()})
+            return response.json();
+        })
         .then(data => {
             // console.log(data.playlists.items);
 
@@ -64,7 +67,7 @@ function queryPlaylists() {
             playlists.forEach(i => {
                 output += `<a href="${i.external_urls.spotify}" target="_blank" title="${i.name}"><img src="${i.images[0].url}" class='m-1' style='height: 150px; width: 150px' onclick='window.location.href='./player.html''/></a>`;
             });
-            $(".playlists").html(output);
+            $(".playlists").html(output); // Renders array of playlists to div
         })
         .catch(err => {
             console.log(err);
