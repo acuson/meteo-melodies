535714013e2aab4b13d478bfff192f7a6f988a33
Christine Nguyen <cnhnguyen59@gmail.com>
2021-12-13 19:44:41 -0500

    style changes

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -97,7 +97,7 @@ function selectedPlaylist(uri, playlistName) {
     $('#title').html(`Playing "${playlistName}"`)
 
     $(".player").html(
-        `<iframe src="https://open.spotify.com/embed/${uri}" width="100%" height="250px" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+        `<iframe class='mt=2' src="https://open.spotify.com/embed/${uri}" width="100%" height="350px" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
         
 
     );
